6b02ad2d4fb949c10bee0ea11f06d2e2d3e5b49f
Temuulen <temuulenkhuchit01@gmail.com>
2024-10-11 20:03:06 -0500

    "Updated script.js: added comments explaining execution context and lexical environment, and added code examples for array and object manipulation using various methods."

--- ExamPrepration/script.js
+++ ExamPrepration/script.js
@@ -12,7 +12,74 @@ const getNewArray = function (arr) {
 // console.log(getNewArray(["Hello", "Wonderful", "Happy", "People", "Have a great day"]));
 
 /*
-this: 
-LE[{}], tdz{}, closure{}
+1. Global EC creation:
+  outer: null, this: Window
+  LE[{ a: undefined, outer: fn }], tdz{ b }
 
+2. Global EC execution:
+  outer: null, this: Window
+  LE[{ a: 2, outer: fn, b: 3 }], tdz{ }
+
+3. outer EC creation:
+  outer: global, this: undefined
+  LE[{ d: undefined }], tdz { c }
+
+4. outer EC execution:
+  outer: global, this: undefined
+  LE[{ d: 7, c: 5, inner: fn }], tdz{ }
+
+5. inner EC creation:
+  outer: outer, this: undefined
+  LE[{ }], tdz { c }, closure(outer):{d:7}
+
+6. inner EC execution:
+  outer: outer, this: undefined
+  LE[{ c: 9 }], tdz{ }, closure(outer):{d:7}
+
+  
 */
+let arr = [10, 20, 30, 40, 50];
+
+// arr.splice(2, 0, 25);
+// console.log(arr);
+
+// arr.filter((n) => n % 15 == 0).forEach((n) => console.log(n));
+
+// console.log(arr.map((n) => n * n));
+
+// console.log(arr.reduce((a, b) => a + b, 0));
+
+// console.log(arr.find((n) => n > 40));
+
+const students = [
+  { name: "Quincy", grades: [99, 88], courses: ["cs301", "cs303"] },
+  { name: "Jason", grades: [29, 38], courses: ["cs201", "cs203"] },
+  { name: "Alexis", grades: [79, 78], courses: ["cs105", "cs211"] },
+  { name: "Sam", grades: [91, 82], courses: ["cs445", "cs303"] },
+  { name: "Katie", grades: [66, 77], courses: ["cs303", "cs477"] },
+];
+
+// const result = students
+//   .filter((s) => s.courses.includes("cs303"))
+//   .reduce((acc, s) => {
+//     let avg = s.grades.reduce((sum, grade) => sum + grade, 0) / s.grades.length;
+//     acc[s.name] = avg;
+//     return acc;
+//   }, {});
+// console.log(result);
+
+const employees = [
+  { name: "Alice", salary: 90000, department: "IT", teams: ["engineering", "devOps"] },
+  { name: "Bob", salary: 70000, department: "IT", teams: ["support"] },
+  { name: "Charlie", salary: 120000, department: "Engineering", teams: ["engineering", "devOps"] },
+  { name: "Dave", salary: 180000, department: "Engineering", teams: ["engineering"] },
+  { name: "Eve", salary: 95000, department: "HR", teams: ["recruitment"] },
+];
+
+const result = employees
+  .filter((e) => e.teams.includes("engineering"))
+  .reduce((acc, e) => {
+    acc[e.department] = (acc[e.department] || 0) + e.salary;
+    return acc;
+  }, {});
+console.log(result);
