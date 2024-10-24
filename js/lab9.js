d4aa6197a9543b6ec36393e183cf1b4260311fc0
Temuulen <temuulenkhuchit01@gmail.com>
2024-10-23 22:10:43 -0500

    Update

--- js/lab9.js
+++ js/lab9.js
@@ -1,5 +1,3 @@
-"use strict";
-
 // Question 1.
 import fs from "fs";
 
@@ -21,32 +19,32 @@ fs.readFile("./files/input.txt", "utf-8", (err, data) => {
 });
 
 // Question 2.
-// const http = require("http");
+import { createServer } from "http";
 
-// const server = http.createServer((req, res) => {
-//   if (req.method === "GET") {
-//     if (req.url === "/image") {
-//       res.writeHead(200, { "Content-Type": "image/jpeg" });
-//       fs.createReadStream("./path-to-image.jpg").pipe(res);
-//     } else if (req.url === "/pdf") {
-//       res.writeHead(200, { "Content-Type": "application/pdf" });
-//       fs.createReadStream("./path-to-pdf.pdf").pipe(res);
-//     } else if (req.url === "/about") {
-//       res.writeHead(200, { "Content-Type": "text/plain" });
-//       fs.createReadStream("./path-to-about.txt").pipe(res);
-//     } else if (req.url === "/home" || req.url === "/") {
-//       res.writeHead(200, { "Content-Type": "text/plain" });
-//       res.end("Welcome to my website");
-//     } else {
-//       res.writeHead(404, { "Content-Type": "text/plain" });
-//       res.end("404 Not Found");
-//     }
-//   } else {
-//     res.writeHead(405, { "Content-Type": "text/plain" });
-//     res.end("Method Not Allowed");
-//   }
-// });
+const server = createServer((req, res) => {
+  if (req.method === "GET") {
+    if (req.url === "/image") {
+      res.writeHead(200, { "Content-Type": "image/jpeg" });
+      fs.createReadStream("./path-to-image.jpg").pipe(res);
+    } else if (req.url === "/pdf") {
+      res.writeHead(200, { "Content-Type": "application/pdf" });
+      fs.createReadStream("./path-to-pdf.pdf").pipe(res);
+    } else if (req.url === "/about") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      fs.createReadStream("./path-to-about.txt").pipe(res);
+    } else if (req.url === "/home" || req.url === "/") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Welcome to my website");
+    } else {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("404 Not Found");
+    }
+  } else {
+    res.writeHead(405, { "Content-Type": "text/plain" });
+    res.end("Method Not Allowed");
+  }
+});
 
-// server.listen(3000, () => {
-//   console.log("Server is running on http://localhost:3000");
-// });
+server.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
